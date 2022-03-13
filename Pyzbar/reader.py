7b4b2785c3b85387fc5d70c0a3c1afdcfbf2a9df
MK3247 <wafulacharles47@gmail.com>
2022-03-13 09:35:10 +0300

    QRcode & Barcode reader

---
+++ Pyzbar/reader.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from pyzbar.pyzbar import decode 
+
+import cv2
+
+img = cv2.imread('code.png')
+
+code = decode(img)
+
+print(code)
+
+for barcode in decode(img):
+
+    print(barcode.data)
+
+    text = barcode.data.decode('utf-8')
+
+    print(text)
+
+    print(barcode.rect) 
---
+++ QR Code/qr.py
@@ -0,0 +1,5 @@
+import qrcode 
+
+img = qrcode.make('Hello World')
+
+img.save('hello.png')
